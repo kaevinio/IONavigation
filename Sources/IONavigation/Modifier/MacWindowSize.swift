e9fc6fed5cd97c38f5b23bdeca8ba11122911570
Kevin Waltz <hello@kaevin.io>
2022-11-16 08:09:22 +0100

    Set maxWidth and maxHeight for macWindowSize

--- Sources/IONavigation/Modifier/MacWindowSize.swift
+++ Sources/IONavigation/Modifier/MacWindowSize.swift
@@ -13,7 +13,7 @@ public struct MacWindowSize: ViewModifier {
     public func body(content: Content) -> some View {
         content
             #if os(macOS)
-            .frame(minWidth: minWidth, maxWidth: .infinity, minHeight: minHeight, maxHeight: .infinity)
+            .frame(minWidth: minWidth, maxWidth: maxWidth, minHeight: minHeight, maxHeight: maxHeight)
             #endif
     }
     
@@ -22,12 +22,14 @@ public struct MacWindowSize: ViewModifier {
     // MARK: - Varibles
     
     let minWidth: CGFloat
+    let maxWidth: CGFloat
     let minHeight: CGFloat
+    let maxHeight: CGFloat
     
 }
 
 extension View {
-    public func macWindowSize(minWidth: CGFloat = 800, minHeight: CGFloat = 600) -> some View {
-        modifier(MacWindowSize(minWidth: minWidth, minHeight: minHeight))
+    public func macWindowSize(minWidth: CGFloat = 800, maxWidth: CGFloat = .infinity, minHeight: CGFloat = 600, maxHeight: CGFloat = .infinity) -> some View {
+        modifier(MacWindowSize(minWidth: minWidth, maxWidth: maxWidth, minHeight: minHeight, maxHeight: maxHeight))
     }
 }
