13526b5b0fe163cdf60f9ac69b403b248bbff745
Kevin Waltz <hello@kaevin.io>
2024-07-17 20:31:28 +0200

    - FIX: Set colors as properties so they update accordingly

--- Sources/IONavigation/Tabbar/TabbarViewModel.swift
+++ Sources/IONavigation/Tabbar/TabbarViewModel.swift
@@ -14,9 +14,6 @@ public class TabbarViewModel: ObservableObject {
     @Published var items: [Item]
     @Published var selectedItem: Item
     
-    @Published var barColor: Color
-    @Published var itemColor: Color
-    @Published var itemTintColor: Color
     @Published var font: Font?
     @Published var style: TabStyle
     
@@ -24,13 +21,10 @@ public class TabbarViewModel: ObservableObject {
     
     // MARK: - Init
     
-    public init(items: [Item], barColor: Color, itemColor: Color, itemTintColor: Color, font: Font? = nil, style: TabStyle) {
+    public init(items: [Item], font: Font? = nil, style: TabStyle) {
         self.items = items
         self.selectedItem = items.first ?? Item(id: "", title: "", image: Image(""), view: AnyView(Text("")))
         
-        self.barColor = barColor
-        self.itemColor = itemColor
-        self.itemTintColor = itemTintColor
         self.font = font
         self.style = style
     }
