9c5db6caee87ef6e7b23ea59d48a7a11af90dde4
Kevin Waltz <hello@kaevin.io>
2024-09-15 09:48:45 +0200

    - Added option to add background to text in navigationItem

--- Sources/IONavigation/Navigationbar/Modifier/NavigationItem.swift
+++ Sources/IONavigation/Navigationbar/Modifier/NavigationItem.swift
@@ -9,21 +9,43 @@ import SwiftUI
 
 public struct NavigationItem: ViewModifier {
     
+    // MARK: - Variables
+    
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    #endif
+    
+    @State private var isHovering = false
+    
+    let text: String?
+    let font: Font
+    let image: Image?
+    let foregroundColor: Color
+    let backgroundColor: Color
+    let disabled: Bool
+    let tooltip: String?
+    let action: () -> Void
+    
+    
+    
+    // MARK: - Body
+    
     public func body(content: Content) -> some View {
-        HStack(alignment: .bottom, spacing: 0) {
+        HStack(spacing: 0) {
             content
             
             if let text {
                 Button(action: action) {
                     Text(text)
                         .font(font)
-                        .foregroundColor(color)
-                        .padding(Values.middlePadding / 2)
-                        .background(isHovering ? color.opacity(0.1) : color.opacity(0))
-                        .cornerRadius(Values.cornerRadius)
+                        .foregroundStyle(foregroundColor)
+                        .padding(.vertical, Values.minorPadding / 4)
+                        .padding(.horizontal, Values.minorPadding)
+                        .background(isHovering ? backgroundColor.opacity(0.1) : backgroundColor.opacity(1))
+                        .clipShape(Capsule())
                 }
                 .frame(height: Values.navigationBarHeight)
-                .padding(.trailing, Values.minorPadding)
+                .padding(.trailing, Values.middlePadding)
                 .buttonStyle(.plain)
                 .fixedSize()
                 #if os(iOS)
@@ -35,7 +57,7 @@ public struct NavigationItem: ViewModifier {
                 .help(tooltip ?? "")
                 .disabled(disabled)
             } else if let image {
-                NavigationBarButton(icon: image, font: font, color: color, action: action)
+                NavigationBarButton(icon: image, font: font, color: foregroundColor, action: action)
                     .padding(.trailing, Values.minorPadding)
                     #if os(iOS)
                     .padding(.top, horizontalSizeClass == .compact ? 0 : Values.minorPadding)
@@ -48,28 +70,10 @@ public struct NavigationItem: ViewModifier {
         }
     }
     
-    
-    
-    // MARK: - Variables
-    
-    #if os(iOS)
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    #endif
-    
-    @State private var isHovering = false
-    
-    let text: String?
-    let font: Font
-    let image: Image?
-    let color: Color
-    let disabled: Bool
-    let tooltip: String?
-    let action: () -> Void
-    
 }
 
 extension View {
-    public func navigationItem(text: String? = nil, font: Font = .title2, image: Image? = nil, color: Color = .primary, disabled: Bool = false, tooltip: String? = nil, action: @escaping () -> Void) -> some View {
-        modifier(NavigationItem(text: text, font: font, image: image, color: color, disabled: disabled, tooltip: tooltip, action: action))
+    public func navigationItem(text: String? = nil, font: Font = .headline, image: Image? = nil, foregroundColor: Color = .primary, backgroundColor: Color = .clear, disabled: Bool = false, tooltip: String? = nil, action: @escaping () -> Void) -> some View {
+        modifier(NavigationItem(text: text, font: font, image: image, foregroundColor: foregroundColor, backgroundColor: backgroundColor, disabled: disabled, tooltip: tooltip, action: action))
     }
 }
